0547c21b3eeebc48fc609d748d3ae7e389022362
ercos41@gmail.com <ercos41@gmail.com@ba27bda6-bfbe-3134-c131-4311ea68ed2a>
2011-03-15 05:31:27 +0000

    a ver si ahora si deja versionar el proyecto

---
+++ trunk/CSOF5101 Ingeniera de Software/TSP/IngeniumTSP/src/com/ingenium/tsp/annotations/LogTList.java
@@ -0,0 +1,17 @@
+package com.ingenium.tsp.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author Ingenium
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = {ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR})
+public @interface LogTList {
+
+    LogT[] value();
+}
