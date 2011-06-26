7fd4cd81745c61245960eaf97c07c6066feb51de
davidres <davidres@ba27bda6-bfbe-3134-c131-4311ea68ed2a>
2011-06-26 19:09:05 +0000

    ReporteEventos

---
+++ CSOF5204 Arquitectura de Software/workspace/ASHWebModule/src/java/com/ingenium/ash/connection/ManagerConnection.java
@@ -0,0 +1,102 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ingenium.ash.connection;
+
+import com.ingenium.ash.transportdata.Event;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import oracle.jdbc.OracleTypes;
+
+/**
+ * Clase en cargada de ejecutar consultas de los diferentes reportes de la aplicacion
+ * @author Sandra Milena Gómez Ríos
+ */
+public class ManagerConnection {
+
+    /* Conexion con la base de datos */
+    Connection connection;
+    
+    /**
+     * Constructor de la clase, obtiene la conexion con la base de datos
+     */
+    public ManagerConnection() {
+        connection = ConnectionFactory.getConnection();
+    }    
+    
+    /**
+     * Consulta todos los eventos generados
+     * @return List, lista de eventos
+     */
+    public List<Event> eventsGeneratedConsult() {
+
+        String procedure = "{call PROGETREPORTBYHOME(?)}";
+        ResultSet resultSet = null;
+        CallableStatement callableStatement = null;
+        List<Event> result = new ArrayList<Event>();
+
+        try {
+            callableStatement = connection.prepareCall(procedure);
+
+            // Registramos los parametro de salida OUT
+            callableStatement.registerOutParameter(1, OracleTypes.CURSOR);
+            
+            // Ejecutamos el procedimiento
+            callableStatement.execute();
+
+            // Obtenemos los parametros de salida OUT
+            resultSet = (ResultSet) callableStatement.getObject(1);
+            
+            while (resultSet.next()) {
+                Event event = new Event();
+                event.setEventDate(resultSet.getString(1));
+                event.setEventName(resultSet.getString(2));
+                event.setHomeOffice(resultSet.getString(3));
+                result.add(event);
+            }
+            
+        } catch (Exception e) {
+            System.err.println("Error al generar la consulta del reporte de evento generados: "+" : "+e);
+        } 
+        return result;
+    }
+    
+    /**
+     * Consulta todos los eventos generados
+     * @return List, lista de eventos
+     */
+    public List<Event> homeOfficeConsult() {
+
+        String procedure = "{call PROGETREPORTBYHOME(?)}";
+        ResultSet resultSet = null;
+        CallableStatement callableStatement = null;
+        List<Event> result = new ArrayList<Event>();
+
+        try {
+            callableStatement = connection.prepareCall(procedure);
+
+            // Registramos los parametro de salida OUT
+            callableStatement.registerOutParameter(1, OracleTypes.CURSOR);
+            
+            // Ejecutamos el procedimiento
+            callableStatement.execute();
+
+            // Obtenemos los parametros de salida OUT
+            resultSet = (ResultSet) callableStatement.getObject(1);
+            
+            while (resultSet.next()) {
+                Event event = new Event();
+                event.setEventDate(resultSet.getString(1));
+                event.setEventName(resultSet.getString(2));
+                event.setHomeOffice(resultSet.getString(3));
+                result.add(event);
+            }
+            
+        } catch (Exception e) {
+            System.err.println("Error al generar la consulta del reporte de evento generados: "+" : "+e);
+        } 
+        return result;
+    }
+}
---
+++ CSOF5204 Arquitectura de Software/workspace/ASHWebModule/src/java/com/ingenium/ash/report/ReportConsolidatedInformationBean.java
@@ -0,0 +1,49 @@
+package com.ingenium.ash.report;
+
+import com.ingenium.ash.connection.ManagerConnection;
+import com.ingenium.ash.transportdata.Event;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Clase encargada de manejar la lógica de la página que permite consultar la 
+ * informacion consoolidad mostrando todos los eventos registrados.
+ * 
+ * @author Sandra Milena Gómez Ríos
+ */
+public class ReportConsolidatedInformationBean {
+    
+    /* Lista con los eventos */
+    private List<Event> listEvents;
+    /* Administrador de consultar */
+    private ManagerConnection managerConnection;
+    
+    /**
+     * Constructor de la clase 
+     */
+    public ReportConsolidatedInformationBean(){
+        listEvents = new ArrayList<Event>();
+        managerConnection = new ManagerConnection();
+    }
+    
+    /**
+     * Consulta los todos los eventos registrados para la casa u oficina seleccionada.
+     */
+    public void informationConsult(){
+        try{
+           listEvents = new ArrayList<Event>();           
+           listEvents = managerConnection.eventsGeneratedConsult();
+        } catch(Exception ex){
+           System.err.println("Se presentó un error al cargar la lista de casas y oficinas: "+ex);
+        }
+    }
+    
+     /**
+     * Permite obtener la lista de eventos
+     * @return List, lista con los eventos.
+     */
+    public List<Event> getListEvents() {
+        return listEvents;
+    }
+    
+}
