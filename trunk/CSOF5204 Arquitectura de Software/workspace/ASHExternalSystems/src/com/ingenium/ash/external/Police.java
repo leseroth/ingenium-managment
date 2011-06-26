39aa510e215ca47dda07084eeb0b25c2d95837de
ercos41@gmail.com <ercos41@gmail.com@ba27bda6-bfbe-3134-c131-4311ea68ed2a>
2011-06-26 21:17:13 +0000

    agregados los sockets directos a los sistemas de emergencias

---
+++ trunk/CSOF5204 Arquitectura de Software/workspace/ASHExternalSystems/src/com/ingenium/ash/external/Police.java
@@ -0,0 +1,54 @@
+package com.ingenium.ash.external;
+
+import com.ingenium.ash.communication.util.Util;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Erik Arcos
+ */
+public class Police {
+
+    private static ServerSocket serverSocket;
+    private static Socket socket;
+    private static final int port = 4450;
+
+    public static void main(String... args) {
+        startSystem();
+    }
+
+    private static void startSystem() {
+        DataInputStream dataInputStream = null;
+
+        try {
+            serverSocket = new ServerSocket(port);
+            socket = serverSocket.accept();
+            dataInputStream = new DataInputStream(socket.getInputStream());
+        } catch (IOException ex) {
+            Logger.getLogger(Police.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        while (true) {
+            try {
+                byte[] byteTotal = new byte[4];
+                byteTotal[0] = dataInputStream.readByte();
+                byteTotal[1] = dataInputStream.readByte();
+                byteTotal[2] = dataInputStream.readByte();
+                byteTotal[3] = dataInputStream.readByte();
+                int size = Util.byteArrayToInt(byteTotal);
+
+                byte[] byteArray = new byte[size];
+                dataInputStream.read(byteArray);
+                
+                Logger.getLogger(Police.class.getName()).log(Level.WARNING, new String(byteArray));
+            } catch (IOException ex) {
+                Logger.getLogger(Police.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
