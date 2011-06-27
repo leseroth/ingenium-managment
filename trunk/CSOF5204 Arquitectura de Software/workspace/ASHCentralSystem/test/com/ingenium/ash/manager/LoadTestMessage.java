957b0763963c6f91801c2bdd162ccc693d4ded45
widrobo@gmail.com <widrobo@gmail.com@ba27bda6-bfbe-3134-c131-4311ea68ed2a>
2011-06-27 02:31:58 +0000

    add LoadTestMessage.java

---
+++ trunk/CSOF5204 Arquitectura de Software/workspace/ASHCentralSystem/test/com/ingenium/ash/manager/LoadTestMessage.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ingenium.ash.manager;
+
+import com.clarkware.junitperf.TimedTest;
+import junit.framework.Test;
+/**
+ *
+ * @author PC12345
+ */
+public class LoadTestMessage {
+
+    public static Test suite() {
+        long maxTimeInMillis = 80000; //modificar tiempo en milisegundos aceptado
+        int concurrentUsers = 10; //modificar usuarios componentes
+        Test test = new MessageTest("testMessage");
+        Test timedTest = new TimedTest(test, maxTimeInMillis);
+        Test loadTest1 = new com.clarkware.junitperf.LoadTest(timedTest, concurrentUsers);
+        return loadTest1;
+    }
+
+    public static void main(String args[]) {
+
+        junit.textui.TestRunner.run(suite());
+    }
+
+}
