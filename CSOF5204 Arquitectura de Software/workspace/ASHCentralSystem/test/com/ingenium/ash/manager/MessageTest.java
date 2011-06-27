b4c6c574297580498edc18506a28e6eec7d6e161
ercos41 <ercos41@gmail.com>
2011-06-27 02:29:27 +0000

    aja

---
+++ CSOF5204 Arquitectura de Software/workspace/ASHCentralSystem/test/com/ingenium/ash/manager/MessageTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ingenium.ash.manager;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MessageTest extends TestCase {
+    
+    public MessageTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+    
+    public void testMessage() {
+    
+    }
+}
