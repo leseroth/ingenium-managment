7c740e8bdc089a2865d333817161aa67899ccde0
ercos41 <ercos41@gmail.com>
2011-09-10 23:02:08 +0000

    unversioned

--- MarketPlaceIngenium/NetbeansProjects/Facturacion/build/generated/jax-wsCache/facturacionconfirmacionpago_client_ep/com/oracle/xmlns/marketplace_jws/procesofacturacion/facturacionconfirmacionpago/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://xmlns.oracle.com/MarketPlace_jws/ProcesoFacturacion/FacturacionConfirmacionPago", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.oracle.xmlns.marketplace_jws.procesofacturacion.facturacionconfirmacionpago;
