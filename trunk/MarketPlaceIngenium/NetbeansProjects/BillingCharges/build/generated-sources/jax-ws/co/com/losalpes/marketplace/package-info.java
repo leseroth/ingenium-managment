7a6c1e490e05c046d0d00c57756f137b7697aabf
ercos41@gmail.com <ercos41@gmail.com@ba27bda6-bfbe-3134-c131-4311ea68ed2a>
2011-09-10 23:02:08 +0000

    unversioned

--- trunk/MarketPlaceIngenium/NetbeansProjects/BillingCharges/build/generated-sources/jax-ws/co/com/losalpes/marketplace/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://marketplace.losalpes.com.co", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package co.com.losalpes.marketplace;
