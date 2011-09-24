c3bfd7dfa2a7fc641b019809cba2eb1b610b5a0f
ercos41 <ercos41@gmail.com>
2011-09-24 21:09:54 +0000

    Se corrije la aplicacion Mailer, y se agrega un proyecto de soapui

--- MarketPlaceIngenium/NetbeansProjects/Mailer/src/java/co/com/losalpes/marketplace/mailer/beans/MailerEmailManagementRemote.java
+++ MarketPlaceIngenium/NetbeansProjects/Mailer/src/java/co/com/losalpes/marketplace/mailer/beans/MailerEmailManagementRemote.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package co.com.losalpes.marketplace.mailer.beans;
 
 import javax.ejb.Remote;
--- MarketPlaceIngenium/NetbeansProjects/Mailer/src/java/co/com/losalpes/marketplace/mailer/ws/MailSendingManagement.java
+++ MarketPlaceIngenium/NetbeansProjects/Mailer/src/java/co/com/losalpes/marketplace/mailer/ws/MailSendingManagement.java
@@ -1,19 +1,13 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package co.com.losalpes.marketplace.mailer.ws;
 
 import co.com.losalpes.marketplace.mailer.beans.MailerEmailManagementLocal;
-import co.com.losalpes.marketplace.mailer.exceptions.CorreosInvalidosException;
-import co.com.losalpes.marketplace.mailer.exceptions.CorruptedAttachmentException;
-import co.com.losalpes.marketplace.mailer.exceptions.InternalMailServerException;
+import co.com.losalpes.marketplace.mailer.exceptions.BussinessException;
 import javax.ejb.EJB;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.ejb.Stateless;
+import java.util.List;
 
 /**
  *
@@ -22,24 +16,34 @@ import javax.ejb.Stateless;
 @WebService()
 @Stateless()
 public class MailSendingManagement {
+
     @EJB
     private MailerEmailManagementLocal ejbRef;
 
     /**
-     * Web service operation
+     * Envia un correo electronico a las cuentas indicadas
+     * @param to Para
+     * @param cc Con copia
+     * @param bcc Con copia oculta
+     * @param subject Asunto
+     * @param message Mensaje
+     * @param from Cuenta de envio
+     * @param password Password de la cuenta
+     * @param attachments Ruta de los archivos adjuntos
+     * @return null si todos los correos se envian, un String si habia correos erroneos
+     * @throws BussinessException
      */
     @WebMethod(operationName = "sendMail")
-    public Boolean sendMail(@WebParam(name = "to")
-    java.util.List<java.lang.String> to, @WebParam(name = "cc")
-    java.util.List<java.lang.String> cc, @WebParam(name = "bcc")
-    java.util.List<java.lang.String> bcc, @WebParam(name = "subject")
-    String subject, @WebParam(name = "message")
-    String message, @WebParam(name = "from")
-    String from, @WebParam(name = "password")
-    String password, @WebParam(name = "attachments")
-    java.util.List<java.lang.String> attachments) throws CorreosInvalidosException,
-    InternalMailServerException, CorruptedAttachmentException{
+    public String sendMail(
+            @WebParam(name = "to") List<String> to,
+            @WebParam(name = "cc") List<String> cc,
+            @WebParam(name = "bcc") List<String> bcc,
+            @WebParam(name = "subject") String subject,
+            @WebParam(name = "message") String message,
+            @WebParam(name = "from") String from,
+            @WebParam(name = "password") String password,
+            @WebParam(name = "attachments") List<String> attachments)
+            throws BussinessException {
         return ejbRef.sendMail(to, cc, bcc, subject, message, from, password, attachments);
     }
-
 }
