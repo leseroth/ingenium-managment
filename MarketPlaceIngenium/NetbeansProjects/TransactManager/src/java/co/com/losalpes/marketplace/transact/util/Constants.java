2a50c25f2c37ede0a30df67a441ed823293b9b6f
ercos41 <ercos41@gmail.com>
2011-10-09 14:20:30 +0000

    agergar paquete de util que no se dejo agregar en el ultimo commit

---
+++ MarketPlaceIngenium/NetbeansProjects/TransactManager/src/java/co/com/losalpes/marketplace/transact/util/Constants.java
@@ -0,0 +1,43 @@
+package co.com.losalpes.marketplace.transact.util;
+
+/**
+ *
+ * @author Erik
+ */
+public class Constants {
+
+    // PurchaseOrder
+    public static final String NUM_SEGUIMIENTO_SEPARATOR = "-";
+    // Excepciones
+    public static final String EXC_ENTITY_INCOMPLETE = "Falta informacion para crear el ?";
+    public static final String EXC_ENTITY_DETACHED = "No se debe enviar id para ? al momento de crearlo";
+    public static final String EXC_ENTITY_TOO_MUCH_INFO = "No se esperaba el campo ?  en ?";
+    public static final String EXC_ENTITY_INEXSISTENT = "La entidad ? identificada por ? no existe";
+    public static final String EXC_INCORRECT_STATE = "El estado ? no es un estado valido";
+    public static final String EXC_INCORRECT_ARGUMENT = "Los siguientes argumentos tienen valores no validos: ?";
+    public static final String EXC_PO_STATE_UPDATE = "Error en la actualizacion de estado: ?";
+
+    /**
+     * Enumeracion para almacenar los estados posibles de una orden de compra
+     */
+    public enum PurchaseOrderState {
+
+        SolicitadoComercio, AceptadoFabricante, RechazadoFabricante, ProcesadoFabricante,
+        EntregadoFabricante, AceptadoComercio, RechazadoComercio, RecibidoFabricante;
+
+        public static boolean isValidStateChange(PurchaseOrderState before, PurchaseOrderState after) {
+            return before.compareTo(after) < 0;
+        }
+
+        public static PurchaseOrderState getState(String strState) {
+            PurchaseOrderState state = null;
+            for (PurchaseOrderState pos : PurchaseOrderState.values()) {
+                if (pos.toString().equals(strState)) {
+                    state = pos;
+                    break;
+                }
+            }
+            return state;
+        }
+    }
+}
