f45d0fd7a5a96d32edf53921a1c7d2d0f85106fe
ercos41@gmail.com <ercos41@gmail.com@ba27bda6-bfbe-3134-c131-4311ea68ed2a>
2011-10-09 14:20:30 +0000

    agergar paquete de util que no se dejo agregar en el ultimo commit

---
+++ trunk/MarketPlaceIngenium/NetbeansProjects/TransactManager/src/java/co/com/losalpes/marketplace/transact/util/Util.java
@@ -0,0 +1,23 @@
+package co.com.losalpes.marketplace.transact.util;
+
+import co.com.losalpes.marketplace.transact.entities.Comercio;
+import static co.com.losalpes.marketplace.transact.util.Constants.*;
+
+/**
+ *
+ * @author Erik
+ */
+public class Util {
+
+    public static final String getNumSeguimiento(Comercio com) {
+        String numSeguimiento = null;
+        if (com != null) {
+            numSeguimiento = com.getNit() + NUM_SEGUIMIENTO_SEPARATOR + System.currentTimeMillis();
+        }
+        return numSeguimiento;
+    }
+
+    public static boolean isEmptyString(String str) {
+        return str == null || str.trim().length() == 0;
+    }
+}
