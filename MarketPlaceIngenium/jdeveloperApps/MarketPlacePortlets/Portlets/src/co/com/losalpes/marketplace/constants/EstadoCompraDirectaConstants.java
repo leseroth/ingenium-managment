100b803c3318ef7a4c8845a64a53001498067dbb
ercos41 <ercos41@gmail.com>
2011-10-12 12:19:11 +0000

    Se organiza proceso de orden de compra directa

--- MarketPlaceIngenium/jdeveloperApps/MarketPlacePortlets/Portlets/src/co/com/losalpes/marketplace/constants/EstadoCompraDirectaConstants.java
+++ MarketPlaceIngenium/jdeveloperApps/MarketPlacePortlets/Portlets/src/co/com/losalpes/marketplace/constants/EstadoCompraDirectaConstants.java
@@ -4,4 +4,5 @@ public class EstadoCompraDirectaConstants {
     public static final String ENTREGADO_FABRICANTE = "EntregadoFabricante";
     public static final String SOLICITADO_COMERCIO = "SolicitadoComercio";
     public static final String ACEPTADO_FABRICANTE = "AceptadoFabricante";
+    public static final String RECHAZADO_FABRICANTE = "RechazadoFabricante";
 }
