75859db556779a090110dfca8539798fad5500b9
ercos41@gmail.com <ercos41@gmail.com@ba27bda6-bfbe-3134-c131-4311ea68ed2a>
2011-10-12 12:18:46 +0000

    Se organiza proceso de orden de compra directa

--- trunk/MarketPlaceIngenium/jdeveloperApps/MarketPlacePortlets/Portlets/src/co/com/losalpes/marketplace/beans/InfoCompraDirecta.java
+++ trunk/MarketPlaceIngenium/jdeveloperApps/MarketPlacePortlets/Portlets/src/co/com/losalpes/marketplace/beans/InfoCompraDirecta.java
@@ -1,42 +1,69 @@
 package co.com.losalpes.marketplace.beans;
 
 
+import co.com.losalpes.marketplace.constants.EstadoCompraDirectaConstants;
 import co.com.losalpes.marketplace.servicio.ServicioProxy;
+import co.com.losalpes.marketplace.vos.OrdenCompraVO;
 import co.com.losalpes.marketplace.vos.SubastaVO;
 
+import java.security.InvalidParameterException;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
 
 public class InfoCompraDirecta {
-    private boolean fabricante=false;
-    private SubastaVO subasta;
+
+    private OrdenCompraVO compraDirecta;
     private ServicioProxy servProxy;
-    public InfoCompraDirecta() {
-       
-        
-    }
-
-    public String realizarOferta_action() {
+    private Long precio;
+    
+    public InfoCompraDirecta() {                              
         ExternalContext ec=FacesContext.getCurrentInstance().getExternalContext();
-        ec.getRequestMap().put("compraDirecta",subasta);
-        return "crearOferta";
+        if((OrdenCompraVO)ec.getRequestMap().get("compraDirecta")!=null)
+         this.setCompraDirecta( (OrdenCompraVO)ec.getRequestMap().get("compraDirecta"));
     }
 
-    public void setFabricante(boolean fabricante) {
-        this.fabricante = fabricante;
+    public boolean aprobarRechazarCompra(String estado) {
+        boolean creada=false;
+        try{            
+            servProxy.aceptarCompraDirecta(compraDirecta.getNumSeguimiento(), estado, precio);
+            creada=true;
+        }catch(InvalidParameterException ipe){
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("El estado de la orden de compra no fue actualizado "+ipe.getMessage()));
+        }  
+        return creada;
+    }
+    
+    public String aprobar() {
+        if(aprobarRechazarCompra(EstadoCompraDirectaConstants.ACEPTADO_FABRICANTE)){
+              FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Orden de compra aprobada!"));
+        }
+        return "volver";
+    }
+    
+    public String rechazar() {
+        if(aprobarRechazarCompra(EstadoCompraDirectaConstants.RECHAZADO_FABRICANTE)){
+              FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Orden de compra rechazada!"));
+        }
+        return "volver";
     }
 
-    public boolean isFabricante() {
-        return fabricante;
+    public void setPrecio(Long precio) {
+        this.precio = precio;
     }
 
-    public void setSubasta(SubastaVO subasta) {
-        this.subasta = subasta;
+    public Long getPrecio() {
+        return precio;
     }
 
-    public SubastaVO getSubasta() {
-        return subasta;
+    public void setCompraDirecta(OrdenCompraVO compraDirecta) {
+        this.compraDirecta = compraDirecta;
+    }
+
+    public OrdenCompraVO getCompraDirecta() {
+        return compraDirecta;
     }
 
     public String volver_action() {
