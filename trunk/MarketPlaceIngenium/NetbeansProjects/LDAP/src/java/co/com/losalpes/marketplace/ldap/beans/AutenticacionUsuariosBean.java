387d86e9d297a82187d6528183e290c73d6aca27
ercos41@gmail.com <ercos41@gmail.com@ba27bda6-bfbe-3134-c131-4311ea68ed2a>
2011-11-07 03:48:04 +0000

    Se corrigen las referencias a web services

--- trunk/MarketPlaceIngenium/NetbeansProjects/LDAP/src/java/co/com/losalpes/marketplace/ldap/beans/AutenticacionUsuariosBean.java
+++ trunk/MarketPlaceIngenium/NetbeansProjects/LDAP/src/java/co/com/losalpes/marketplace/ldap/beans/AutenticacionUsuariosBean.java
@@ -1,17 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package co.com.losalpes.marketplace.ldap.beans;
 
-import co.com.losalpes.marketplace.gestioncorreoelectronico.GestionCorreoElectronico_Service;
 import co.com.losalpes.marketplace.ldap.bos.UsuarioBO;
 import co.com.losalpes.marketplace.ldap.entities.Usuario;
 import co.com.losalpes.marketplace.ldap.exceptions.EstadoNoExisteException;
 import co.com.losalpes.marketplace.ldap.exceptions.RolNoExisteException;
 import co.com.losalpes.marketplace.ldap.exceptions.UsuarioNoExisteException;
 import co.com.losalpes.marketplace.ldap.utilities.RandomPassword;
+import co.com.losalpes.marketplace.mailer.ws.MailSendingManagementService;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Stateless;
@@ -26,18 +21,23 @@ import javax.xml.ws.WebServiceRef;
  */
 @Stateless
 public class AutenticacionUsuariosBean implements AutenticacionUsuariosRemote, AutenticacionUsuariosLocal {
-    @WebServiceRef(wsdlLocation = "META-INF/wsdl/localhost_7001/marketPlace/proxy/GestionCorreoElectronico.wsdl")
-    private GestionCorreoElectronico_Service service;
+
+    private static final String INGENIUM_EMAIL = "ingenium.uniandes@gmail.com";
+    private static final String INGENIUM_EMAIL_PASSWORD = "ingenium2011";
+    @WebServiceRef(wsdlLocation = "META-INF/wsdl/localhost_8148/MailSendingManagementService/MailSendingManagement.wsdl")
+    private MailSendingManagementService service;
     @PersistenceContext
     private EntityManager em;
 
+    @Override
     public UsuarioBO autenticar(String login, String password) throws UsuarioNoExisteException {
         Query q = em.createNamedQuery("consultarUsuarioLoginPsswd");
         q.setParameter("login", login);
         q.setParameter("pwd", password);
-        List<Usuario> usuario = (List<Usuario>)q.getResultList();
-        if(usuario.isEmpty())
+        List<Usuario> usuario = (List<Usuario>) q.getResultList();
+        if (usuario.isEmpty()) {
             throw new UsuarioNoExisteException("El usuario especificado no existe en el sistema");
+        }
         return usuario.get(0).toBO();
     }
 
@@ -45,14 +45,16 @@ public class AutenticacionUsuariosBean implements AutenticacionUsuariosRemote, A
         em.persist(object);
     }
 
-    public UsuarioBO crearUsuario(String nit, String nombre, String rol, String email) throws RolNoExisteException{
-        if(!rol.equals("Comercio") && !rol.equals("Fabricante") && !rol.equals("MP Reportes") && !rol.equals("MP Consultas"))
-            throw new RolNoExisteException("El rol "+ rol + " no existe en el sistema");
+    @Override
+    public UsuarioBO crearUsuario(String nit, String nombre, String rol, String email) throws RolNoExisteException {
+        if (!rol.equals("Comercio") && !rol.equals("Fabricante") && !rol.equals("MP Reportes") && !rol.equals("MP Consultas")) {
+            throw new RolNoExisteException("El rol " + rol + " no existe en el sistema");
+        }
         Usuario u = new Usuario();
         u.setEstado("Activo");
         u.setNit(nit);
         u.setRol(rol);
-        u.setLogin(nombre.substring(0, 2)+"_"+nit.substring(0,2));
+        u.setLogin(nombre.substring(0, 2) + "_" + nit.substring(0, 2));
         u.setPassword(RandomPassword.getRandomString(10));
         u.setEmail(email);
         persist(u);
@@ -60,65 +62,73 @@ public class AutenticacionUsuariosBean implements AutenticacionUsuariosRemote, A
         return u.toBO();
     }
 
+    @Override
     public Boolean actualizarEstado(String login, String estado) throws UsuarioNoExisteException, EstadoNoExisteException {
-        if(!estado.equals("Activo") || !estado.equals("Inactivo"))
-            throw new EstadoNoExisteException("El estado "+ estado + " no representa un estado válido para el sistema.");
+        if (!estado.equals("Activo") || !estado.equals("Inactivo")) {
+            throw new EstadoNoExisteException("El estado " + estado + " no representa un estado válido para el sistema.");
+        }
         Query q = em.createNamedQuery("consultarUsuarioLogin");
         q.setParameter("login", login);
-        Usuario u = (Usuario)q.getSingleResult();
-        if(u == null)
-            throw new UsuarioNoExisteException("El usuario con el login "+ login + " no existe en el sistema.");
+        Usuario u = (Usuario) q.getSingleResult();
+        if (u == null) {
+            throw new UsuarioNoExisteException("El usuario con el login " + login + " no existe en el sistema.");
+        }
         u.setEstado(estado);
         em.flush();
         return true;
     }
 
+    @Override
     public String obtenerNitUsuario(String login) throws UsuarioNoExisteException {
         Query q = em.createNamedQuery("consultarUsuarioLogin");
         q.setParameter("login", login);
-        List<Usuario> usuarios = (List<Usuario>)q.getResultList();
-        if(usuarios.isEmpty())
+        List<Usuario> usuarios = (List<Usuario>) q.getResultList();
+        if (usuarios.isEmpty()) {
             throw new UsuarioNoExisteException("El usuario con el login " + login + " no existe.");
+        }
         return usuarios.get(0).getNit();
     }
 
+    @Override
     public Boolean cambiarClave(String login, String nuevaClave) throws UsuarioNoExisteException {
         Query q = em.createNamedQuery("consultarUsuarioLogin");
         q.setParameter("login", login);
-        List<Usuario> usuarios = (List<Usuario>)q.getResultList();
-        if(usuarios.isEmpty())
+        List<Usuario> usuarios = (List<Usuario>) q.getResultList();
+        if (usuarios.isEmpty()) {
             throw new UsuarioNoExisteException("El usuario con el login " + login + " no existe.");
+        }
         usuarios.get(0).setPassword(nuevaClave);
         em.flush();
         return true;
     }
 
-    public Boolean recuperarClave(String login, String email) throws UsuarioNoExisteException{
+    @Override
+    public Boolean recuperarClave(String login, String email) throws UsuarioNoExisteException {
         Query q = em.createNamedQuery("consultarUsuarioLogin");
         q.setParameter("login", login);
-        List<Usuario> usuarios = (List<Usuario>)q.getResultList();
-        if(usuarios.isEmpty())
+        List<Usuario> usuarios = (List<Usuario>) q.getResultList();
+        if (usuarios.isEmpty()) {
             throw new UsuarioNoExisteException("El usuario con el login " + login + " no existe.");
-        if(!usuarios.get(0).getEmail().equals(email))
-            throw new UsuarioNoExisteException("El usuario con el login "+login+" no posee el email especificado.");
+        }
+        if (!usuarios.get(0).getEmail().equals(email)) {
+            throw new UsuarioNoExisteException("El usuario con el login " + login + " no posee el email especificado.");
+        }
         String newPassword = RandomPassword.getRandomString(10);
         usuarios.get(0).setPassword(newPassword);
 
         try { // Call Web Service Operation
-            co.com.losalpes.marketplace.gestioncorreoelectronico.GestionCorreoElectronico port = service.getGestionCorreoElectronicoSOAP();
+            co.com.losalpes.marketplace.mailer.ws.MailSendingManagement port = service.getMailSendingManagementPort();
+
             java.util.List<java.lang.String> to = new ArrayList<String>();
             to.add(email);
-            java.lang.String from = "info@marketplace.losalpes.com.co";
-            java.lang.String password = "marketplace2011";
             java.lang.String subject = "Recuperación de clave";
-            java.lang.String message = "Estimado "+login+":\n\nLa nueva clave para ingresar al portal es "+newPassword
-                    +".\n\nCordialmente,\n\nEl equipo del MarketPlace de los Alpes";
-            boolean result = port.enviarCorreoElectronico(to, null, null, from, password, subject, message, null);
-            System.out.println("Result = "+result);
+            java.lang.String message = "Estimado " + login + ":\n\nLa nueva clave para ingresar al portal es " + newPassword + ".\n\nCordialmente,\n\nEl equipo del MarketPlace de los Alpes";
+
+            port.sendMail(to, null, null, subject, message, INGENIUM_EMAIL, INGENIUM_EMAIL_PASSWORD, null);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
+        
         return true;
     }
 }
