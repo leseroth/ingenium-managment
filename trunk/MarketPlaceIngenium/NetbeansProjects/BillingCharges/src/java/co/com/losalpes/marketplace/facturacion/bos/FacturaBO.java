d737bde4412981ce911768d59e815c1be9ab5819
ercos41@gmail.com <ercos41@gmail.com@ba27bda6-bfbe-3134-c131-4311ea68ed2a>
2011-11-19 22:47:56 +0000

    Crear cuenta de facturacion

--- trunk/MarketPlaceIngenium/NetbeansProjects/BillingCharges/src/java/co/com/losalpes/marketplace/facturacion/bos/FacturaBO.java
+++ trunk/MarketPlaceIngenium/NetbeansProjects/BillingCharges/src/java/co/com/losalpes/marketplace/facturacion/bos/FacturaBO.java
@@ -1,94 +1,104 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package co.com.losalpes.marketplace.facturacion.bos;
 
+import co.com.losalpes.marketplace.facturacion.MarketPlaceBO;
 import java.io.Serializable;
 import java.util.Date;
 
-/**
- *
- * @author marketplace
- */
-public class FacturaBO implements Serializable{
+public class FacturaBO implements MarketPlaceBO, Serializable {
 
     private Long id;
-
-    /**
-     * Attribute estado
-     */
-
-    private String url;
-
+    private Date fechaFin;
+    private Date fechaInicio;
     private String nombreArchivo;
-
+    private String url;
     private Long valorPago;
 
-    private Date fechaInicio;
-
-    private Date fechaFin;
-
     /**
-     * Default Constructor
+     * Constructor por defecto
      */
     public FacturaBO() {
-
     }
 
     /**
-     * Simple Constructor
+     * @return the id
      */
-    public FacturaBO(Long id, String url) {
-        this.id = id;
-        this.url = url;
-    }    
-
     public Long getId() {
         return id;
     }
 
+    /**
+     * @param id the id to set
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getNombreArchivo() {
-        return nombreArchivo;
-    }
-
-    public void setNombreArchivo(String nombreArchivo) {
-        this.nombreArchivo = nombreArchivo;
-    }
-
+    /**
+     * @return the fechaFin
+     */
     public Date getFechaFin() {
         return fechaFin;
     }
 
+    /**
+     * @param fechaFin the fechaFin to set
+     */
     public void setFechaFin(Date fechaFin) {
         this.fechaFin = fechaFin;
     }
 
+    /**
+     * @return the fechaInicio
+     */
     public Date getFechaInicio() {
         return fechaInicio;
     }
 
+    /**
+     * @param fechaInicio the fechaInicio to set
+     */
     public void setFechaInicio(Date fechaInicio) {
         this.fechaInicio = fechaInicio;
     }
 
+    /**
+     * @return the nombreArchivo
+     */
+    public String getNombreArchivo() {
+        return nombreArchivo;
+    }
+
+    /**
+     * @param nombreArchivo the nombreArchivo to set
+     */
+    public void setNombreArchivo(String nombreArchivo) {
+        this.nombreArchivo = nombreArchivo;
+    }
+
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @param url the url to set
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * @return the valorPago
+     */
     public Long getValorPago() {
         return valorPago;
     }
 
+    /**
+     * @param valorPago the valorPago to set
+     */
     public void setValorPago(Long valorPago) {
         this.valorPago = valorPago;
     }
